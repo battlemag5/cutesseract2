bb43e2003153a51d44403fd9bb894c1a4477a447
M-K-S-P <miha.kokh@gmail.com>
2026-04-15 11:05:42 +0700

    fml

--- tests/test_main.cu
+++ tests/test_main.cu
@@ -7,7 +7,7 @@
 #include "test_class_matrix.cu"
 
 #ifndef RUNS_NUM
-#define RUNS_NUM 1024
+#define RUNS_NUM 4
 #endif
 
 using std::cout;
@@ -15,7 +15,7 @@ using std::endl;
 using std::vector;
 
 template <typename T>
-T froebenius(Matrix<T> A, Matrix<T> B) {
+T sum_sq_diff(Matrix<T> A, Matrix<T> B) {
     A.cpu();
     B.cpu();
     assert(A.rows() == B.rows());
@@ -31,12 +31,13 @@ T froebenius(Matrix<T> A, Matrix<T> B) {
     return std::abs(sumA - sumB); 
 }
 
-std::vector<std::pair<unsigned, unsigned> > diffs(Matrix<T> A, Matrix<T> B) {
+template <typename T>
+std::vector<std::pair<size_t, size_t> > diffs(Matrix<T> A, Matrix<T> B) {
     A.cpu();
     B.cpu();
     assert(A.rows() == B.rows());
     assert(A.cols() == B.cols());
-    vector<std::pair<unsigned, unsigned> > diffs;
+    vector<std::pair<size_t, size_t> > diffs;
     for (size_t i = 0; i < A.rows(); i++) {
         for (size_t j = 0; j < B.cols(); j++) {
             if (std::abs(A.get(i, j) - B.get(i, j)) > 1e-3) {
@@ -46,7 +47,7 @@ std::vector<std::pair<unsigned, unsigned> > diffs(Matrix<T> A, Matrix<T> B) {
     }
     return diffs;
 }
-
+template <typename T>
 Matrix<T> mmul_cpu(Matrix<T> A, Matrix<T> B) {
     assert(A.cols() == B.rows());
     Matrix<T> C(A.rows(), B.cols(), ROW_WISE, CPU);
@@ -63,7 +64,7 @@ Matrix<T> mmul_cpu(Matrix<T> A, Matrix<T> B) {
 }
 
 
-signed test(unsigned rows1, unsigned cols1, unsigned rows2, unsigned cols2, (void) (*mul_function)(Matrix<T>, Matrix<T>, Matrix<T>), T precision = 1e-3) {
+signed test(unsigned rows1, unsigned cols1, unsigned rows2, unsigned cols2, (void) (*mul_function)(Matrix<T>, Matrix<T>, Matrix<T>)) {
     assert(rows1 == cols2 && "геи\n");
     unsigned runs = RUNS_NUM;
         for (unsigned i = 0; i < runs; i++) {
@@ -78,15 +79,23 @@ signed test(unsigned rows1, unsigned cols1, unsigned rows2, unsigned cols2, (voi
             B.cpu();
             Matrix<fp32> C = mmul_cpu(A, B);
     
-            auto diff = froebenius(C, G);
-            if (diff > precision) {
+            auto diff = sum_sq_diff(C, G);
+            if (diff > 1e-3) {
                 auto diffs_ij = diffs(C, G);
-                cout << "Differing indices: ";
+                cout << "Differing indices | diff " << endl;
                 for (auto& p : diffs_ij) {
-                    cout << "(" << p.first << ", " << p.second << ") ";
+                    cout << p.first << " " << p.second  << " | " << std::abs(C.get(p.first, p.second) - G.get(p.first, p.second)) << endl;
                 }
                 cout << endl;
             }
         }
+    return 0;
+}
+
+signed main() {
+    test(128, 128, 128, 128, __gemm_nn_block_launcher);
+    test(256, 128, 128, 256, __gemm_nn_block_launcher);
+    test(512, 512, 512, 512, __gemm_nn_block_launcher);
+    test(1024, 1024, 1024, 1024, __gemm_nn_block_launcher);
     return 0;
 }
